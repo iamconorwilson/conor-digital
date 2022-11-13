52ef950948160cca4a914560e78d84747b660633
Conor Wilson <iamconorwilson@gmail.com>
2022-11-13 13:26:31 +0000

    Homepage updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
+function replaceAt(str,index,replace) {
+    if (index > str.length - 1 || index < 0) return str;
+    return str.substring(0, index) + replace + str.substring(index + 1);
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     const responseText = document.getElementById('listen')
     const response = await fetch('.netlify/functions/spotify').then(res => res.json())
-    responseText.innerText = response.artist;
+    let artistName = replaceAt(response.artist, response.artist.lastIndexOf(" "), "&nbsp;");
+    responseText.innerHTML = artistName;
     responseText.href = response.url;
 })
