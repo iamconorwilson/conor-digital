3b09faa45791d98e484e4014d02b73ac048bc4bd
Conor Wilson <iamconorwilson@gmail.com>
2023-12-19 21:55:07 +0000

    Style and Function Updates
    * Changed Font to Helvetica
    * Minor spacing tweaks
    * Refreshed Spotify token

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,18 @@ function replaceAt(str,index,replace) {
 
 document.addEventListener('DOMContentLoaded', async () => {
     const responseText = document.getElementById('listen')
-    const response = await fetch('.netlify/functions/spotify').then(res => res.json())
+    const response = await fetch('.netlify/functions/spotify')
+    .then(res => res.json())
+    .catch(err => {
+        console.log(err);
+        return {
+            artist: 'Radiohead',
+            url: 'https://open.spotify.com/artist/4Z8W4fKeB5YxbusRsdQVPb'
+        }
+    })
+
+
+
     let artistName = replaceAt(response.artist, response.artist.lastIndexOf(" "), "&nbsp;");
     responseText.innerHTML = artistName;
     responseText.href = response.url;
